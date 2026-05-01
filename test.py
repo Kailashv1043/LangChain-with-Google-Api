d9f80fd6ef96b5f09edae632c220b42592d3f96d
Kailash <kailash@aekaadvisors.com>
2026-05-01 06:20:17 +0000

    Deleted test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyBDsXfJp3BRFNderJ-XaQGnv1ahaACCZkk")
-
-for m in genai.list_models():
-    print(m.name, m.supported_generation_methods)
